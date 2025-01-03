05ee1fee2e7ad08ab03408e96a4d90ee3ba30eda
TLEWA <2156477725@qq.com>
2025-01-03 18:42:16 +0800

    更改 merge 流程

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 
 gulp.task('styles', function () {
-  return gulp.src(['styles/header.styl',"1.styl"]) // 入口文件
+  return gulp.src([
+      'styles/header.styl',
+      "styles/Tools/*"
+    ]) // 入口文件
     .pipe(concat('all.styl'))
     .pipe(gulp.dest('./merged')); // 输出目录
 });
