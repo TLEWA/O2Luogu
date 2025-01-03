02b3c353352aa38a4a536edd1d62dcad6ae06222
TLEWA <2156477725@qq.com>
2025-01-03 20:04:25 +0800

    初步文件架构

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,10 @@ const concat = require('gulp-concat');
 gulp.task('styles', function () {
   return gulp.src([
       'styles/header.styl',
-      "styles/Tools/*"
+      "styles/Defines/*.styl",
+      "styles/Tools/string.styl",
+      "styles/Tools/selector.styl",
+      "styles/Tools/*.styl"
     ]) // 入口文件
     .pipe(concat('all.styl'))
     .pipe(gulp.dest('./merged')); // 输出目录
