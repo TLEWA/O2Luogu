e6a8a495e7705d86fe2c9818cdd9483ca6c3a78a
TLEWA <2156477725@qq.com>
2025-01-22 15:02:23 +0800

    增加文件导入语法

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat');
+const gulp = require('gulp'); // 引入 gulp
+const concat = require('gulp-concat'); // 文件合并操作
+const fileInclude = require('gulp-file-include'); // 文件导入操作
 
 gulp.task('styles', function () {
   return gulp.src([
@@ -13,5 +14,9 @@ gulp.task('styles', function () {
       "styles/Tools/*.styl"
     ]) // 入口文件
     .pipe(concat('all.styl'))
+    .pipe(fileInclude({
+      prefix: '@@',  // 设置文件导入标记的前缀
+      basepath: 'lib'  // 定义基础路径
+    }))
     .pipe(gulp.dest('./merged')); // 输出目录
 });
